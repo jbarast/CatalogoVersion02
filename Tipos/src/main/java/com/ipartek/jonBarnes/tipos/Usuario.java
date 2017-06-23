165ba061f34df401835a6457d074e8facc58da17
jbarast <jbarast@gmail.com>
2017-06-23 16:16:17 +0200

    Empezando de nuevo el projecto

---
+++ Tipos/src/main/java/com/ipartek/jonBarnes/tipos/Usuario.java
@@ -0,0 +1,16 @@
+package com.ipartek.jonBarnes.tipos;
+
+/**
+ * POJO usuario, para guardar datos de lo usuario.
+ * 
+ * @author jonBarnes
+ * @version 23/06/2017
+ *
+ */
+public class Usuario {
+
+	public Usuario() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
