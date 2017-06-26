9c59deaeb7a13e28d1a7e7471771c134f1c552f7
jbarast <jbarast@gmail.com>
2017-06-26 19:44:53 +0200

    Ultima version subida del 26/06/2017 @jbarast

---
+++ DAL/src/main/java/com/ipartek/jonBarnes/DAL/interfaces/ProductoInterfaceDAO.java
@@ -0,0 +1,14 @@
+//ProductoInterfaceDAO.java
+
+package com.ipartek.jonBarnes.DAL.interfaces;
+
+/**
+ * Interfaz para la clase Producto.
+ * 
+ * @author jonBarnes
+ * @version 26/06/2017
+ *
+ */
+public class ProductoInterfaceDAO {
+
+}
