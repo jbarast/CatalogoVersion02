9fc7ad643b9e3a30418bed34a189d20739b8c05e
jbarast <jbarast@gmail.com>
2017-07-19 17:03:27 +0200

    Commit antes de la rama, para probar como funciona el Set<> en vez de List<>
    @jbarast

---
+++ DAL/src/main/java/com/ipartek/jonBarnes/DAL/pruebas/ProbandoRelacionFacturaProducto.java
@@ -0,0 +1,14 @@
+//ProbandoRelacionFacturaProducto.java
+
+package com.ipartek.jonBarnes.DAL.pruebas;
+
+/**
+ * Clase para hacer pruebas.
+ *
+ * @author jbarnes
+ * @version 14/07/2017
+ *
+ * Created by JAVA on 14/07/2017.
+ */
+public class ProbandoRelacionFacturaProducto {
+}
