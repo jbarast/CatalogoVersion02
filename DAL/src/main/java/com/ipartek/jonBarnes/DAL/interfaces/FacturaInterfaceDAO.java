ae32ee7b62c7d9061fe33c71030e55105e0cc80e
jbarast <jbarast@gmail.com>
2017-07-20 15:18:04 +0200

    Interfaz para DAO Facturas realizada. --Meter cantidad a la relacion many to many. --Hacer la DAO.
    @jbarast

--- DAL/src/main/java/com/ipartek/jonBarnes/DAL/interfaces/FacturaInterfaceDAO.java
+++ DAL/src/main/java/com/ipartek/jonBarnes/DAL/interfaces/FacturaInterfaceDAO.java
@@ -1,4 +1,34 @@
+//FacturaInterfaceDAO.java
+
 package com.ipartek.jonBarnes.DAL.interfaces;
 
+
+import com.ipartek.jonBarnes.tipos.Factura;
+import com.ipartek.jonBarnes.tipos.Usuario;
+
+/**
+ * Interfaz para el DAO de facturas.
+ *
+ * @author jonBarnes
+ * @version 20/07/2017
+ */
 public interface FacturaInterfaceDAO {
+
+    // Añadir una factura a la tabla.
+    public void insert(Factura factura);
+
+    // Borrar una factura de la tabla
+    public void delete(Factura factura);
+
+    // Mostrar la lista de facturas.
+    public Factura[] findAll();
+
+    //Mostrar lista de facturas de un usuario.
+    public Factura[] findFacturasCliente(Usuario cliente);
+
+    // Buscar por id.
+    public Factura findByID(Long id);
+
+    // Buscar por factura.
+    public Factura finByRol(final String factura);
 }
